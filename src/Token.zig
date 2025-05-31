dcb1eef4de90ee5b44b363118b4e8ee1da4dac4a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-31 19:11:28 +0200

    sketch tokenizer

--- src/Token.zig
+++ src/Token.zig
@@ -6,6 +6,7 @@ pub const Token = union(enum) {
     RightParen: void,
     Identifier: []const u8,
     Directive: []const u8,
+    EndOfFile: void,
 };
 
 pub const Lexer = struct {
@@ -22,5 +23,31 @@ pub const Lexer = struct {
     pub fn tokenize(self: *Lexer, allocator: Allocator) std.ArrayList(Token) {
         _ = self;
         return std.ArrayList(Token).init(allocator);
+    }
+
+    fn peek(self: *const Lexer) u8 {
+        return self.input[self.head];
+    }
+
+    fn advance(self: *Lexer) void {
+        if (!self.eof()) {
+            self.head += 1;
+        }
+    }
+
+    fn eof(self: *const Lexer) bool {
+        return self.head == self.input.len;
+    }
+
+    fn get(self: *const Lexer) u8 {
+        const out = self.input[self.head];
+        self.advance();
+        return out;
+    }
+
+    fn readToken(self: *Lexer) !Token {
+        if (self.eof()) {
+            return Token.EndOfFile;
+        }
     }
 };
