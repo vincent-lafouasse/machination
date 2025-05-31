423a7be3e462842e80c5cb943d3a89e2b101630c
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-05-31 18:44:01 +0200

    extract String struct

--- src/main.zig
+++ src/main.zig
@@ -1,44 +1,7 @@
 const std = @import("std");
-const Token = @import("Token.zig").Token;
 const Allocator = std.mem.Allocator;
 
-const String = struct {
-    data: std.ArrayList(u8),
-
-    const Self = @This();
-
-    pub fn init(allocator: Allocator) Self {
-        const data = std.ArrayList(u8).init(allocator);
-
-        return Self{ .data = data };
-    }
-
-    pub fn deinit(self: *Self) void {
-        self.data.deinit();
-    }
-
-    pub fn readline(reader: anytype, allocator: Allocator) !String {
-        var out = String.init(allocator);
-
-        try reader.streamUntilDelimiter(out.data.writer(), '\n', null);
-
-        return out;
-    }
-
-    pub fn format(
-        self: *const Self,
-        comptime fmt: []const u8,
-        options: std.fmt.FormatOptions,
-        writer: anytype,
-    ) !void {
-        _ = fmt;
-        _ = options;
-
-        for (self.data.items) |c| {
-            try writer.print("{c}", .{c});
-        }
-    }
-};
+const String = @import("String.zig").String;
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
